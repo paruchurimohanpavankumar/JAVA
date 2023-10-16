50f3d9bd9da0158c256b03d6111c13f8497fb123
paruchurimohanpavankumar <116320024+paruchurimohanpavankumar@users.noreply.github.com>
2023-10-16 11:28:05 -0700

    Add files via upload

---
+++ If_else_statment.java
@@ -0,0 +1,42 @@
+public class If_else_statment {
+    public static void main(String[] args) {
+
+
+   /*     int a=100;
+        int b=120;
+        if (a>b)
+            System.out.println("A has the higher value");
+        else
+            System.out.println("B is the higher value");
+
+    */
+
+
+
+    }
+}
+
+
+/*                                                     NOTES
+
+IF and SWITCH Statements are the Selection Statements
+                                                     if Statement
+         Syntax:
+         if(condition)                                                          if(condition){
+         statement 1;        =>if there are more than one line then                 Statement-1 line one
+         else                   then we have create a block using                   Statement-1   line two
+         statement 2;             curly braces                                                }
+                                                                                else{
+                                                                                    Statement 2 line one
+                                                                                    Statement 2 line two
+                                                                                      }
+** if both statements are false then both will execute.
+
+** condition vs expression
+condition is the thing that is TURE or False.
+Expression any piece of code which the ending ";" (semicolon).
+
+
+
+
+ */
