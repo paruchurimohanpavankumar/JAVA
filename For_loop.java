e25a33913ea1030f0c67e421aea64c386a5ac23d
paruchurimohanpavankumar <116320024+paruchurimohanpavankumar@users.noreply.github.com>
2023-10-20 13:05:54 -0700

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,71 @@
+
+
+
+public class For_loop {
+    public static void main(String[] args) {
+      /*  int n=5;
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+ " ="+n*i);
+            }
+
+
+
+        ///////////////////////////////////////////////////////////////////////
+        int n=143258;
+        for(int i=100000;i>0;i/=10){
+            int b=(n/i)%10;
+            System.out.println(b);
+        }
+
+        ////////////////////////////////////////////////////////////////////////
+           int n=10;
+           int f=0,s=1,t;
+        System.out.println(f);
+        System.out.println(s);
+        for(int i=2;i<=n;i++){
+            t=f+s;
+            System.out.println(t);
+            f=s;
+            s=t;
+          }
+          ///////////////////////////////////////////////////////////////////////////
+
+        int n = 1456;
+        int result = 0;
+        for (; n != 0; n /= 10) {
+            int last=n%10;
+            result=result*10+last;
+            System.out.println(result);
+
+        }
+        System.out.println(result);
+        ///////////////////////////////////////////////////////////////////////////////////
+
+
+        String word="mohan";
+        String r="";
+        for(int i=word.length()-1;i>=0;i--){
+            r=r+word.charAt(i);
+
+            }
+        System.out.println(r);
+        //////////////////////////////////////////////////////////////////////////////////////////
+       */
+
+        }
+
+    }
+/*
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+it executes limited amount of times
+they are two forms of for loop they are
+1.for
+2.for-each
+
+they are three portions in the for loop
+1. initialization-this is an expression that set the value of the loop control variable
+2.condition-This must be boolean
+3.iteration-it is an expression that increment or decrement loop control
+ */
